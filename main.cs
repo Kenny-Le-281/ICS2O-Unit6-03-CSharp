e83a142daf21b082a86a5efea8502f7493e5035a
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-20 14:27:36 +0000

    changes in main.cs

--- main.cs
+++ main.cs
@@ -9,7 +9,7 @@ using System.Text.Json.Nodes;
 class Program {
     public static async Task Main()
     {
-        
+
         HttpClient client = new HttpClient();
         string response = await client.GetStringAsync(
             "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
